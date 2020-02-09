71b4c04dc2fe31ae1e656c4efe2774859822045f
dreamtohu <42732375+dreamtohu@users.noreply.github.com>
2020-02-09 16:43:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if [ -f master ];then
 	rm master
 fi
 
-wget  https://codeload.github.com/drdeng/Robocup2dInstall/zip/master
+wget  https://codeload.github.com/dreamtohu/Robocup2dInstall/zip/master
 sudo apt-get install -y unzip
 unzip  master
 cd  Robocup2dInstall-master
